596c2684166f509128ac586bc349b8e4d90eb31c
cantikapuspitasari <puspitasaricantika150@gmail.com>
2026-06-17 18:53:57 +0700

    update foto dari google drive ke supabase storage

--- next.config.js
+++ next.config.js
@@ -1,31 +1,35 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'ui-avatars.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'randomuser.me',
-      },
-      {
-        protocol: 'https',
-        hostname: 'drive.google.com',
-      },
-    ],
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
-
+/** @type {import('next').NextConfig} */  
+const nextConfig = {  
+  images: {  
+    remotePatterns: [  
+      {  
+        protocol: 'https',  
+        hostname: 'lh3.googleusercontent.com',  
+      },  
+      {  
+        protocol: 'https',  
+        hostname: 'ui-avatars.com',  
+      },  
+      {  
+        protocol: 'https',  
+        hostname: 'randomuser.me',  
+      },  
+      {  
+        protocol: 'https',  
+        hostname: 'drive.google.com',  
+      },  
+      {  
+        protocol: 'https',  
+        hostname: 'bhanbqlcydwfmfifehkk.supabase.co',  
+      },  
+    ],  
+  },  
+  typescript: {  
+    ignoreBuildErrors: true,  
+  },  
+  eslint: {  
+    ignoreDuringBuilds: true,  
+  },  
+};  
+  
 module.exports = nextConfig;
